655c6121be93371596541a50a786ad2c046dd3ab
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-10 09:20:14 +0530

    if else started

--- firstCode.cpp
+++ firstCode.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
-cout<< 2+ 3;
+// cout<< 2+ 3 << endl;
+// cout << 5+5;
+int b;
+   char a = 'c';
+   b = a;
+cout << b;     
+// cout << "Write your 1 sum ";
+// cin >> a;
+// cout << "Write your 2 sum ";
+// cin >> b;
+
+// cout <<"your total sum is: " ;
+// cout << ( a+b);
+
+
 }
+
