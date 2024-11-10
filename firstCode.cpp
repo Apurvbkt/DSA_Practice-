d45c512368b938146fe1584314b8bd3601a43bb7
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-11-10 20:01:25 +0530

    DSA final push

--- firstCode.cpp
+++ firstCode.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main(){
 // cout<< 2+ 3 << endl;
 // cout << 5+5;
-int b;
-   char a = 'c';
-   b = a;
-cout << b;     
+// int b;
+//    char a = 'c';
+//    b = a;
+// cout << b;     
 // cout << "Write your 1 sum ";
 // cin >> a;
 // cout << "Write your 2 sum ";
@@ -14,6 +14,32 @@ cout << b;
 
 // cout <<"your total sum is: " ;
 // cout << ( a+b);
+
+
+// int marks;
+// cout << "enter your marks : ";
+// cin >> marks;
+
+// if(marks >= 60){
+//     cout << "you are pass";
+// }
+// else{
+//     cout << "You are fail" ;
+// }
+
+
+// for(int i = 1; i <= 6; i++){
+//     cout << i*i << endl;
+// }
+
+int n;
+cout << "write your n : ";
+cin >> n;
+
+
+for(int i = 2; i <=n; i = i + 2){
+    cout << i << endl;
+}
 
 
 }
